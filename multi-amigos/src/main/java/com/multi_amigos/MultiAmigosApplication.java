97989d7092689f556c2eb522cd844550916e3e36
Jonath Oliveira <akiles22a@gmail.com>
2026-01-03 16:41:19 -0300

    Estrutura Inicial Projeto

---
+++ multi-amigos/src/main/java/com/multi_amigos/MultiAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.multi_amigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAmigosApplication.class, args);
+	}
+
+}
---
+++ multi-amigos/src/test/java/com/multi_amigos/MultiAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi_amigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
